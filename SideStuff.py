e84af85d3a393b6d62473bb7457497f74e37b16a
KamiDomi <KamiDomi369@gmail.com>
2020-09-10 18:07:12 -0500

    did a bunch of stuff

--- SideStuff.py
+++ SideStuff.py
@@ -17,5 +17,45 @@ class player:
           self.hp = 0
           self.mp = 0
           self.status_effects = []
+          self.location = ""
 player1 = player()
 
+#Title Screen#
+def title_screen_selections():
+    option =input("> ")
+    if option.lower() == ("play"):
+        start_game()
+    elif option.lower() == ("help"):
+        help_menu()
+    elif option.lower() == ("quit"):
+        sys.exit()
+    while option.lower() not in ("play", "help", "quit"):
+        print("please enter a vailed command")
+        option = input("> ")
+        if option.lower() == ("play"):
+            start_game()
+        elif option.lower() == ("help"):
+            help_menu()
+        elif option.lower() == ("quit"):
+            sys.exit()
+
+def title_screen():
+    os.system("clear")
+    print("########################################")
+    print("# Welcome To This fun and exciting rpg #")
+    print("########################################")
+    print("               -play-                   ")
+    print("               -help-                   ")
+    print("               -quit-                   ")
+    title_screen_selections()
+
+def help_menu():
+    print("########################################")
+    print("######### How can i help you? #########")
+    print("########################################")
+    print("               -play-                   ")
+    print("               -help-                   ")
+    print("               -quit-                   ")
+    title_screen_selections()
+
+def start_game():
