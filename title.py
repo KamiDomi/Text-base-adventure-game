364722eedd968687582be21e0a8dafb50330ab5d
KamiDomi <KamiDomi369@gmail.com>
2020-09-10 17:24:07 -0500

    added comment import varible class

--- title.py
+++ title.py
@@ -1,1 +1,18 @@
+#Text based Rpg
+#By KamiDomi
+
+import cmd
+import textwrap
+import sys
+import os
+import time
+import random
+
+screen_width = 100
+
+class Character:
+      def __init__ (name, weapon, character_class):
+          name = name
+          weapon = weapon
+          character_class = character_class
 
